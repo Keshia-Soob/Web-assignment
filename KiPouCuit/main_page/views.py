39fea4a9daae3310c41aab812f2de732ce6f3516
khushiR8 <beedassykhushi@gmail.com>
2025-09-20 23:38:24 +0400

    Refactor project structure: update template paths, enhance styling, and create new base layout

--- KiPouCuit/main_page/views.py
+++ KiPouCuit/main_page/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def landing(request):
-    return render(request, 'main_page/base.html')  # default title KiPouCuit
-def index(request):
-    return render(request, 'main_page/index.html')
-
-def about(request):
-    return render(request, 'main_page/about.html')  # create this template
+def home(request):
+    return render(request, 'main_page/home.html')  # default title KiPouCuit
