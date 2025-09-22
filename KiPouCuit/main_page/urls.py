8405d0d267b1f0ab4b9303a06c903bb10a840397
khushiR8 <beedassykhushi@gmail.com>
2025-09-22 17:23:19 +0400

    added homepage, edited base.html and global css

--- KiPouCuit/main_page/urls.py
+++ KiPouCuit/main_page/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),   # root page shows KiPouCuit
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
--- KiPouCuit/main_page/views.py
+++ KiPouCuit/main_page/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'main_page/home.html')  # default title KiPouCuit
+
+def about(request):
+    return render(request, 'main_page/about.html')
+
+def contact(request):
+    return render(request, 'main_page/contact.html')
