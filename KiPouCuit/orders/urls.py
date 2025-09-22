77d5ae6003b28de2419d95c6ea3a79c7c4d2cced
nusayhah <nusayhahjalal1@gmail.com>
2025-09-23 01:38:10 +0400

    Add order_confirmed html and css, updated views and urls, added one image for order_confirmed

--- KiPouCuit/orders/urls.py
+++ KiPouCuit/orders/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('order/', views.order, name='order'),  # this is the named URL
+    path('order_confirmed/', views.order_confirmed, name='order_confirmed'),
 ]
