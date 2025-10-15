37c8a8b56078e58b071567344d3a325259529aa7
Keshia-Soob <keshiasoobrayen0@gmail.com>
2025-10-16 01:56:02 +0400

    added logout option

--- KiPouCuit/users/urls.py
+++ KiPouCuit/users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('forgot-password/', views.forgot_password_view, name='forgot_password'),
     path('user_history/', views.user_history_view, name='user_history'),
     path('user_profile/', views.user_profile_view, name='user_profile'),
+    path('logout/', views.logout_view, name='logout'),
 ]
