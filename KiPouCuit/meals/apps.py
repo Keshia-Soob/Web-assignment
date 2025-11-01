4cf9fd30080cafdea9cf3f3153a2b27b3b4d4435
khushiR8 <beedassykhushi@gmail.com>
2025-11-01 18:00:26 +0400

    Added list of meals in db, modified gitignore and menu.html (removed categories)

--- KiPouCuit/meals/apps.py
+++ KiPouCuit/meals/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
 
 class MealsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'meals'
+
+    def ready(self):
+        from .models import MenuItem
+        def populate(sender, **kwargs):
+            if not MenuItem.objects.exists():
+            
+                pass
+        post_migrate.connect(populate, sender=self)
