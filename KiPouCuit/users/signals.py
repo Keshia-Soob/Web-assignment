a8ab7fb78d8d13584aa3efa389b733ddcc71aefe
bhaveshguptar1-sys <bhavesh.guptar1@umail.uom.ac.mu>
2025-11-09 15:52:00 +0400

    fix problem with address and phone number not displaying in user profile

--- KiPouCuit/users/signals.py
+++ KiPouCuit/users/signals.py
@@ -3,7 +3,7 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import UserProfile
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance, phone='N/A', address='N/A')
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         UserProfile.objects.create(user=instance, phone='N/A', address='N/A')
