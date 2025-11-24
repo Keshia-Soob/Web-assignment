a06771b0af4ebbaec3f1dc3258385e2f1fdcf944
Haadiya04 <haadiya.manreddy@gmail.com>
2025-11-24 22:38:22 +0400

    added constraint in reviews: user needs to have more than 1 order history to post a review. Implemented auto generated fields in homecook

--- KiPouCuit/reviews/models.py
+++ KiPouCuit/reviews/models.py
@@ -6,6 +6,7 @@ class Review(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
+    # phone = models.CharField(max_length=15, blank=True, null=True)
     rating = models.IntegerField(choices=[(i, f"{i} Star") for i in range(1, 6)])
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
